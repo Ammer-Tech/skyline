c484aebadd02ce60e76a6314836ae3fff9495db7
sidohin-felix <sidohin.felix@gmail.com>
2024-07-13 10:43:43 +0200

    added comments in the todo

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -45,6 +45,7 @@ func init() {
 	ctx, stop = signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
 }
 
+// TODO: This is the part that calls Office365 - probably some re-engineering is needed in this function.
 func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
 	msg, _ := mail.ReadMessage(bytes.NewReader(data))
 	subject := msg.Header.Get("Subject")
@@ -52,6 +53,7 @@ func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
 	return nil
 }
 
+// TODO: Test this that it works.
 func authHandler(remoteAddr net.Addr, mechanism string, username []byte, password []byte, shared []byte) (bool, error) {
 	if string(username[:]) == globalConfig.BasicAuthConfig.Username && string(password[:]) == globalConfig.BasicAuthConfig.Password {
 		return true, nil
